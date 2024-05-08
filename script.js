f1a1c55ea73b9c2423f2379168ac39f7d9d217a4
Alex <o.pruzhanskiy@gmail.com>
2024-05-08 22:07:49 +0300

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let shape = {
+//     type: '',
+//     getArea() {
+//
+//     }
+// };
+// let circle = {
+//     radius: '',
+//     getArea() {
+//         return Math.PI * this.radius ** 2;
+//     }
+// };
+// circle.__proto__ = shape;
+// circle.radius = 15;
+// alert(shape.getArea())
